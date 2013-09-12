c425d1d2717d7a7077187af2eebc4dbbe70dd999
nickkjordan <nkj7@case.edu>
2013-09-12 12:15:32 -0400

    Added App icon files.  Also created basic classes for HomeView and FavoritesView Controllers

---
+++ WRUW/FavoritesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FavoritesViewController.h
+//  WRUW
+//
+//  Created by Nick Jordan on 9/10/13.
+//  Copyright (c) 2013 Nick Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FavoritesViewController : UIViewController
+
+@end
