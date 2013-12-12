7df478112964196596511a2d4bf1125b7158316d
nickkjordan <nkj7@case.edu>
2013-12-12 01:50:17 -0500

    Outlets for container view and segmented control.  #19

---
+++ WRUW/SegmentedViewController.h
@@ -0,0 +1,16 @@
+//
+//  SegmentedViewController.h
+//  WRUW
+//
+//  Created by Nick Jordan on 12/12/13.
+//  Copyright (c) 2013 Nick Jordan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegmentedViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIView *conatinerView;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *favoritesItem;
+
+@end
---
+++ WRUW/SegmentedViewController.m
@@ -0,0 +1,38 @@
+//
+//  SegmentedViewController.m
+//  WRUW
+//
+//  Created by Nick Jordan on 12/12/13.
+//  Copyright (c) 2013 Nick Jordan. All rights reserved.
+//
+
+#import "SegmentedViewController.h"
+
+@interface SegmentedViewController ()
+
+@end
+
+@implementation SegmentedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
