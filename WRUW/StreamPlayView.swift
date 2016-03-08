8a6248eed1a864758caf5484c654e6ced11a5b8d
Nick Jordan <nkj7@case.edu>
2016-03-07 21:28:29 -0500

    git issues... fixed.

--- WRUW/StreamPlayView.swift
+++ WRUW/StreamPlayView.swift
@@ -7,33 +7,30 @@ import MediaPlayer
     // current stream url = "http://wruw-stream2.wruw.org:8000/stream256.mp3"
     let urlAddress = "http://wruw-stream2.wruw.org:8000/stream256.mp3"
 
-
-
-
-
-    }()
     var viewModel: StreamPlayViewModel!
 
     override init(frame: CGRect) {
         super.init(frame: frame)
 
-    override func didMoveToSuperview() {
         viewModel = StreamPlayViewModel(streamPath: urlAddress)
         self.addSubview(animatedPlayPauseButton)
     }
-    
+
+    required init?(coder aDecoder: NSCoder) { fatalError() }
+
+    private lazy var animatedPlayPauseButton: AnimatedButton = {
+        return AnimatedButton(
+            frame: CGRectMake(10, 10, 150, 150),
+            delegate: self.viewModel
+        )
+            .onTap(target: self, selector: "didTapPlayer")
+    }()
+
+    func didTapPlayer() {
+        viewModel.changePlayerStatus()
+    }
+
     @objc func didAppear() {
         animatedPlayPauseButton.didAppear()
-    }
-
-
-        )
-
-    override func canBecomeFirstResponder() -> Bool {
-        return true;
-    }
-
-    }
-
     }
 }
