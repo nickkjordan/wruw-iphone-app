fe5c924ac1fa32b74535a10d61f84ad5bf23c089
Nick Jordan <nkj7@case.edu>
2016-03-14 18:00:24 -0400

    updated stream address

--- WRUW/StreamPlayView.swift
+++ WRUW/StreamPlayView.swift
@@ -3,9 +3,7 @@ import AVFoundation
 import MediaPlayer
 
 @objc class StreamPlayView: UIView  {
-    // previous stream url = "http://wruw-stream.wruw.org:443/stream.mp3"
-    // current stream url = "http://wruw-stream2.wruw.org:8000/stream256.mp3"
-    let urlAddress = "http://wruw-stream2.wruw.org:8000/stream256.mp3"
+    let urlAddress = "http://wruw-stream.wruw.org:8000/stream128.mp3"
 
     var viewModel: StreamPlayViewModel!
 
