5674cba6552477588ae254aa8ca572c88c226eb5
Nick Jordan <nkj7@case.edu>
2018-03-10 14:31:07 -0500

    add wruw router tests

---
+++ WRUWTests/WruwApiRouterTests.swift
@@ -0,0 +1,60 @@
+import XCTest
+@testable import WRUWModule
+
+class WruwApiRouterTests: XCTestCase {
+    let path = "/path"
+    let params = NSDictionary(dictionary: ["key": "value"])
+
+    func testPathInit() {
+        let router = WruwApiRouter(path: path, parameters: nil)
+
+        XCTAssertEqual(router.path, path)
+    }
+
+    func testParametersInit() {
+        let router = WruwApiRouter(path: "", parameters: params)
+
+        XCTAssertEqual(router.parameters, params)
+    }
+
+    func testUrlRequest() {
+        let router = WruwApiRouter(path: path, parameters: params)
+
+        let urlRequest = router.URLRequest
+
+        let url = urlRequest.URL?.absoluteURL
+
+        XCTAssertNotNil(url)
+
+        let fullExpectedUrlString = router.baseUrlString + path + "?key=value"
+        let expectedUrl = NSURL(string: fullExpectedUrlString)
+
+        XCTAssertEqual(url, expectedUrl)
+    }
+
+    func testMultipleParamUrlEncoding() {
+        let params = NSDictionary(dictionary: ["showname": "emergency-donuts", "date": "2016-02-01"])
+
+        let router = WruwApiRouter(path: path, parameters: params)
+
+        let url = router.URLRequest.URL?.absoluteURL
+
+        let fullExpectedUrlString = router.baseUrlString +
+            path +
+            "?showname=emergency-donuts&date=2016-02-01"
+
+        let expectedUrl = NSURL(string: fullExpectedUrlString)
+
+        XCTAssertEqual(url?.path, expectedUrl?.path)
+        XCTAssertEqual(url?.host, url?.host)
+
+        XCTAssertEqual((url?.sortedQueries)!, (expectedUrl?.sortedQueries)!)
+    }
+}
+
+private extension NSURL {
+    var sortedQueries: [String]? {
+        return query?.componentsSeparatedByString("&")
+            .sort()
+    }
+}
